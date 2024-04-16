1edcf46da5563f351e4514d47cdb58b08f8723d6
lauris <lauris@mobilly.lv>
2024-04-16 15:48:04 +0300

    Prepare for publishing an update

--- update.sh
+++ update.sh
@@ -1,20 +1,8 @@
+# Define the update key to make the updates distinguishable.
+UPDATE_KEY=$(date +%s)
+
 # Create bundle.
-expo export --experimental-bundle --output-dir dist
+npx expo export --output-dir dist/$UPDATE_KEY
 
-# Copy meta information about the update.
-cp app.json dist && cp package.json dist
-
-# Archive the bundle.
-cd dist
-zip -q update.zip -r ./*
-cd -
-
-# Upload the bundle.
-curl --location --request POST 'http://localhost:3000/upload' \
---form "uri=@dist/update.zip" \
---header "project: react-native" \
---header "version: 1.0.0" \
---header "release-channel: main" \
---header "upload-key: abc123def456" \
---header "git-branch: $(git rev-parse --abbrev-ref HEAD)" \
---header "git-commit: $(git log --oneline -n 1)"
+# Add Expo configuration.
+node exportClientExpoConfig.js > dist/$UPDATE_KEY/expoConfig.json
