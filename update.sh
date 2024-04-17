390e52ceb1fd3b7628e4b9fb8b2b95743093a7f2
lauris <lauris@mobilly.lv>
2024-04-17 13:42:46 +0300

    Implement ZIP upload with Base64 string

--- update.sh
+++ update.sh
@@ -6,3 +6,18 @@ npx expo export --output-dir dist/$UPDATE_KEY
 
 # Add Expo configuration.
 node exportClientExpoConfig.js > dist/$UPDATE_KEY/expoConfig.json
+
+# Zip the bundle.
+cd dist
+zip update.zip -r ./$UPDATE_KEY
+
+# Upload the bundle as a Base64 encoded string of the ZIP file.
+base64 -i update.zip | curl --location 'http://localhost:3000/upload' \
+--header 'expo-app-name: react-native' \
+--header 'expo-release-channel: main' \
+--header 'expo-runtime-version: 1.0.0' \
+--header 'Content-Type: text/plain' \
+--data @-
+
+# Clean up.
+rm -rf update.zip $UPDATE_KEY
