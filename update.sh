828357d6234d7a1ece0e6a71fa8e019ab9725d3b
lauris <lauris@mobilly.lv>
2024-04-18 10:43:16 +0300

    Generate Expo config on server

--- update.sh
+++ update.sh
@@ -4,19 +4,21 @@ UPDATE_KEY=$(date +%s)
 # Create bundle.
 npx expo export --output-dir dist/$UPDATE_KEY
 
-# Add Expo configuration.
-node exportClientExpoConfig.js > dist/$UPDATE_KEY/expoConfig.json
+# Add meta information about the app.
+cp app.json dist/$UPDATE_KEY/app.json
+cp package.json dist/$UPDATE_KEY/package.json
 
 # Zip the bundle.
 cd dist
 zip update.zip -r ./$UPDATE_KEY
 
 # Upload the bundle as a Base64 encoded string of the ZIP file.
-base64 -i update.zip | curl --location 'http://localhost:3000/upload' \
---header 'expo-app-name: react-native' \
---header 'expo-release-channel: main' \
---header 'expo-runtime-version: 1.0.0' \
---header 'Content-Type: text/plain' \
+base64 -i update.zip | curl --location "http://localhost:3000/upload" \
+--header "expo-app-name: react-native" \
+--header "expo-release-channel: main" \
+--header "expo-runtime-version: 1.0.0" \
+--header "expo-update-id: $UPDATE_KEY" \
+--header "Content-Type: text/plain" \
 --data @-
 
 # Clean up.
